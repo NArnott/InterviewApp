45e29256692ba827ea8a8935edad45d55fff9d94
Nathan Arnott <narnott@gopfs.com>
2020-11-16 14:38:32 -0700

    angular site

---
+++ MySite/ClientApp/src/app/addresses/addresses.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addresses',
+  templateUrl: './addresses.component.html'
+})
+export class AddressesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
